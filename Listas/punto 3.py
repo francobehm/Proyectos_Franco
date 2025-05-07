817897916934a772f4dcb47fd40f022c055b1576
Franco Behm <francobehm22@gmail.com>
2025-05-07 16:19:24 -0300

    se agrego un archivo a listas

---
+++ Listas/punto 3.py
@@ -0,0 +1,8 @@
+# Pedir los datos al usuario
+nombre = input("Ingresa tu nombre: ")
+apellido = input("Ingresa tu apellido: ")
+edad = input("Ingresa tu edad: ")
+residencia = input("Ingresa tu lugar de residencia: ")
+
+# Mostrar la información usando f-string
+print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}.")
